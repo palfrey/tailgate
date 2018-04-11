873dc9cd2a8129ddf6c537e580a59641ac25c6e2
Tom Parker <palfrey@tevp.net>
2018-04-11 22:36:00 +0100

    Also search author lists, as sometimes there's good data there

--- author_list.py
+++ author_list.py
@@ -5,13 +5,38 @@ import logging
 def get_books(key, author_obj):
     all_books = {}
 
-    page = 1
     def parse_val(val):
         if val == None:
             return 1
         else:
             return int(val)
 
+    # Some authors have a good author list
+    page = 1
+    while True:
+        url = "https://www.goodreads.com/author/list/%s?format=xml&key=%s&page=%d" % (author_obj.id, key, page)
+        print(url)
+        author_list = requests.get(url)
+        author_list.raise_for_status()
+        tree = ElementTree.fromstring(author_list.content)
+        books = tree.findall("./author/books/book")
+        for book in books:
+            #raise Exception(ElementTree.tostring(book))
+            title = book.find("title").text
+            when = datetime.date(parse_val(book.find("publication_year").text), parse_val(book.find("publication_month").text), parse_val(book.find("publication_day").text))
+            if when == datetime.date(1,1,1):
+                continue
+            if when == None:
+                raise Exception(book)
+            if title not in all_books or (when !=None and all_books[title]["when"] > when):
+                all_books[title] = {"when": when, "id": int(book.find("id").text)}
+        if len(books) == 30:
+            page +=1
+        else:
+            break
+
+    # ... and some you need to search for
+    page = 1
     while True:
         logging.debug("Page %d", page)
         url = "https://www.goodreads.com/search/index.xml?q=%s&search=author&page=%s&key=%s" % (author_obj.name, page, key)
