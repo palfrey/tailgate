b373f1e9299ea663f5a2629dc0a9807f6c0bad2f
Tom Parker-Shemilt <palfrey@tevp.net>
2018-10-31 21:32:54 +0000

    Use webpack to get all the static files

---
+++ assets.js
@@ -0,0 +1,10 @@
+const CSS = [
+    'bootstrap/dist/css/bootstrap.min.css',
+    'bootstrap/dist/css/bootstrap-theme.min.css'
+];
+const JS = [
+    'jquery/jquery.min.js',
+    'bootstrap/dist/js/bootstrap.min.js'
+];
+
+module.exports = [...JS, ...CSS];
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const webpack = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+// assets.js
+const Assets = require('./assets');
+
+module.exports = {
+    entry: {
+        app: "./templates/base.html",
+    },
+    module: {
+        rules: [
+          { test: /\.html/, use: ['html-loader'] }
+        ]
+    },
+    output: {
+        path: __dirname + "/static/",
+        //filename: "[name].bundle.js"
+    },
+    plugins: [
+      new CopyWebpackPlugin(
+        Assets.map(asset => {
+          return {
+            from: path.resolve(__dirname, `./node_modules/${asset}`),
+            to: path.resolve(__dirname, './static')
+          };
+        })
+      )
+    ]
+};
